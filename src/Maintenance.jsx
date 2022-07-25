8a968dbaee4f38db743e23156cb1c86ce2811ab6
BobTheBuilderSTD <108704846+BobTheBuilderSTD@users.noreply.github.com>
2022-07-25 13:41:55 -0500

    Added subtitle span
    added

--- src/Maintenance.jsx
+++ src/Maintenance.jsx
@@ -20,6 +20,7 @@ export default function Maintenance() {
           <Navbar.Brand href="#home" className="navbar-brand">
             <img alt="" src={DingocoinLogo} />
             <span>DINGOCOIN</span>
+	   <span className="navbar-brand-subtitle"> WRAP</span>
           </Navbar.Brand> 
         </Container>
       </Navbar>
