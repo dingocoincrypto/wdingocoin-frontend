6e2d1057b2c268a5315f9a2a39770d7fcace9736
Twinky-kms <Twinkykms@gmail.com>
2023-03-12 12:25:24 -0400

    Add check if we should show testnet networks (#2)
    * add testnet check

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ import Maintenance from "./Maintenance";
 import MumbaiController from "./MumbaiController";
 
 export default function App() {
- // GA Analytics, Uncomment below, add your GA Measurement ID 	
-{/*
+  // GA Analytics, Uncomment below, add your GA Measurement ID 	
+  {/*
   ReactGA.initialize("ID-HERE");  <-- Enter GA Measurement ID Here
   ReactGA.pageview(window.location.pathname + window.location.search);
 
@@ -35,143 +35,147 @@ export default function App() {
   }, []);
   React.useEffect(() => {}, [location]); */}
 
-const [controller, setController] = React.useState(null);
-  
-{/* Maint Mode Toggle  'const maintenance' = -- true = on  | false = off */}
-  const maintenance = false; 
+  const [controller, setController] = React.useState(null);
+
+  {/* Maint Mode Toggle  'const maintenance' = -- true = on  | false = off */ }
+  const maintenance = false;
+  const testnetEnabled = true;
   return (
     <Router>
       {maintenance ? (
         <Maintenance />
       ) : (
-              <div className="App">
-        <Navbar className="navbar" bg="dark" expand="lg" sticky="top">
-          <Container>
-            <Navbar.Brand href="/" className="navbar-brand align-items-center">
-              <img alt="" src={DingocoinLogo} />
-              <span>DINGOCOIN</span>
-              <span className="navbar-brand-subtitle"> Wrap</span>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse>
-            <Nav className="ms-auto">
-              <Nav.Link
-                href="https://www.dingocoin.org"
-                target="_blank"
-                rel="noreferrer"
-              >
-              <b>Visit Dingocoin</b>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <header id="home" className="masthead">
-        <Container>
-          {/* Desktop view */}
-          <div className="d-none d-lg-block">
-            <div className="d-flex flex-row py-5">
-              <div className="d-flex flex-column me-auto my-4">
-                <h4 className="title text-center">
-                  Wrap Dingocoin with SOL or BSC
-                </h4>
-                <h6 className="text-center">
-                  Stable, Realiable & Great Exchange Rate 
-                </h6>
+        <div className="App">
+          <Navbar className="navbar" bg="dark" expand="lg" sticky="top">
+            <Container>
+              <Navbar.Brand href="/" className="navbar-brand align-items-center">
+                <img alt="" src={DingocoinLogo} />
+                <span>DINGOCOIN</span>
+                <span className="navbar-brand-subtitle"> Wrap</span>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse>
+                <Nav className="ms-auto">
+                  <Nav.Link
+                    href="https://www.dingocoin.org"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <b>Visit Dingocoin</b>
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <header id="home" className="masthead">
+            <Container>
+              {/* Desktop view */}
+              <div className="d-none d-lg-block">
+                <div className="d-flex flex-row py-5">
+                  <div className="d-flex flex-column me-auto my-4">
+                    <h4 className="title text-center">
+                      Wrap Dingocoin with SOL or BSC
+                    </h4>
+                    <h6 className="text-center">
+                      Stable, Realiable & Great Exchange Rate
+                    </h6>
+                  </div>
+                </div>
               </div>
-            </div>
-           </div>
-          {/* Mobile */}
-          <div className="d-lg-none">
-            <div className="d-flex flex-column py-5">
-              <div className="d-flex flex-column me-auto mt-4 mb-auto">
-                <h2 className="title text-center">
-                  Wrap Dingocoin with SOL or BSC
-                </h2><br />
-                <h4 className="text-center">
-                  Stable, Realiable & Great Exchange Rate 
-                </h4>
-              
+              {/* Mobile */}
+              <div className="d-lg-none">
+                <div className="d-flex flex-column py-5">
+                  <div className="d-flex flex-column me-auto mt-4 mb-auto">
+                    <h2 className="title text-center">
+                      Wrap Dingocoin with SOL or BSC
+                    </h2><br />
+                    <h4 className="text-center">
+                      Stable, Realiable & Great Exchange Rate
+                    </h4>
+
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </Container>
-       </header>
-       <section className="features">
-        <Container className="py-5 mt-5">
-          <FadeInSection>
-            <h2 className="mb-3 text-center">wDingocoin Custodian</h2>
-            <h5 className="mb-3 text-center"><strong>What Are Wrapped Coins?</strong></h5>
-            <h5 className="mb-3 ">
-            <p>A wrapped crypto token is a converted version of a cryptocurrency or asset that functions on a network other 
-            than the original asset’s blockchain. Each wrapped token has the same value as the asset it represents and is 
-            easily interchangeable.</p>
-            <p>Wrapped Coins essentially represent crypto assets on non-native blockchains. These coins 
-            are “wrapped” because they are inserted into a wrapper or digital vault that allows the wrapped version to operate 
-            on a different blockchain.
-           </p>
-           </h5>
-            <br />
-            <h5 className="mb-3 text-center">Exchange Rate</h5>
-            <br />
-            <h5 className="mb-3 text-center">
-            <p className="mb-0">
-              1 wDingocoin = 1 Dingocoin
-            </p>
-            </h5>
-            <br />
-            <h5 className="mb-3 text-center">
-            <DropdownButton
-              title={
-                controller === null
-                  ? "Select Wrapped Network"
-                  : controller
-              }
-              className="mb-2"
-            >
-              <Dropdown.Item
-                onClick={() => {
-                  setController("Binance Smart Chain (BSC)");
-                }}
-              >
-                Binance Smart Chain (BSC)
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() => {
-                  setController("Mumbai TESTNET");
-                }}
-              >
-                wtDingo (Mumbai)
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() => {
-                  setController("Solana (SOL)");
-                }}
-              >
-                Solana (SOL)
-              </Dropdown.Item>
-            </DropdownButton>
-          </h5>
-	      </FadeInSection>
-        </Container>
-      </section>  
-      <section>
-		<Container>
-		<h5 className="mt-3 text-center">
-        {controller === "Binance Smart Chain (BSC)" && <BscController />}
-        {controller === "Mumbai TESTNET" && <MumbaiController />}
-      	{controller === "Solana (SOL)" && <SolController />}
-        </h5>
-        </Container>
-      </section>
-       <section className="section-footer text-center">
+            </Container>
+          </header>
+          <section className="features">
+            <Container className="py-5 mt-5">
+              <FadeInSection>
+                <h2 className="mb-3 text-center">wDingocoin Custodian</h2>
+                <h5 className="mb-3 text-center"><strong>What Are Wrapped Coins?</strong></h5>
+                <h5 className="mb-3 ">
+                  <p>A wrapped crypto token is a converted version of a cryptocurrency or asset that functions on a network other
+                    than the original asset’s blockchain. Each wrapped token has the same value as the asset it represents and is
+                    easily interchangeable.</p>
+                  <p>Wrapped Coins essentially represent crypto assets on non-native blockchains. These coins
+                    are “wrapped” because they are inserted into a wrapper or digital vault that allows the wrapped version to operate
+                    on a different blockchain.
+                  </p>
+                </h5>
+                <br />
+                <h5 className="mb-3 text-center">Exchange Rate</h5>
+                <br />
+                <h5 className="mb-3 text-center">
+                  <p className="mb-0">
+                    1 wDingocoin = 1 Dingocoin
+                  </p>
+                </h5>
+                <br />
+                <h5 className="mb-3 text-center">
+                  <DropdownButton
+                    title={
+                      controller === null
+                        ? "Select Wrapped Network"
+                        : controller
+                    }
+                    className="mb-2"
+                  >
+                    <Dropdown.Item
+                      onClick={() => {
+                        setController("Binance Smart Chain (BSC)");
+                      }}
+                    >
+                      Binance Smart Chain (BSC)
+                    </Dropdown.Item>
+                    {
+                      testnetEnabled ?
+                        <Dropdown.Item
+                          onClick={() => {
+                            setController("Mumbai TESTNET");
+                          }}
+                        >
+                          wtDingo (Mumbai)
+                        </Dropdown.Item>
+                        : null}
+                    <Dropdown.Item
+                      onClick={() => {
+                        setController("Solana (SOL)");
+                      }}
+                    >
+                      Solana (SOL)
+                    </Dropdown.Item>
+                  </DropdownButton>
+                </h5>
+              </FadeInSection>
+            </Container>
+          </section>
+          <section>
+            <Container>
+              <h5 className="mt-3 text-center">
+                {controller === "Binance Smart Chain (BSC)" && <BscController />}
+                {controller === "Mumbai TESTNET" && <MumbaiController />}
+                {controller === "Solana (SOL)" && <SolController />}
+              </h5>
+            </Container>
+          </section>
+          <section className="section-footer text-center">
             <Row>
               <span>
-                <b>© The Dingocoin Project 2021 - 2022</b> 
+                <b>© The Dingocoin Project 2021 - 2022</b>
               </span>
             </Row>
-        </section>
-      </div>
+          </section>
+        </div>
       )}
     </Router>
   );
